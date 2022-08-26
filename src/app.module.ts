374db553e26d49e04990f3406f6bb9d537e9ae5e
David Espinoza <deerhou@gmail.com>
2022-08-25 19:41:13 -0500

    file structure base

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './modules/auth/auth.module';
+import { SeedModule } from './modules/seed/seed.module';
+import { CommonModule } from './modules/common/common.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, SeedModule, CommonModule],
   controllers: [],
   providers: [],
 })
