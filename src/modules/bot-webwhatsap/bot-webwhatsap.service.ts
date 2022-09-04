4baa7934aaefe057f13c735c5f3b221a95bfb3b2
David Espinoza <deerhou@gmail.com>
2022-09-04 18:36:09 -0500

    agregando respuestas

--- src/modules/bot-webwhatsap/bot-webwhatsap.service.ts
+++ src/modules/bot-webwhatsap/bot-webwhatsap.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
 
-import { Client, ClientSession } from 'whatsapp-web.js';
+import WAWebJS, {
+  Client,
+  ClientSession,
+  LocalAuth,
+  MessageMedia,
+} from 'whatsapp-web.js';
 import * as qrcode from 'qrcode-terminal';
 import * as fs from 'fs';
+import moment from 'moment';
+
+export const FORMATDATE = 'dd-MM-YYYY hh:mm';
 
 @Injectable()
 export class BotWebwhatsapService {
@@ -14,51 +22,84 @@ export class BotWebwhatsapService {
   // constructor() {}
 
   async qrcode() {
-    this.client = new Client({});
-    console.log(fs.existsSync(this.SESSION_FILE_PATH));
+    // fs.existsSync(this.SESSION_FILE_PATH)
+    //   ? this.whitSession()
+    //   : this.whitOutSession();
+    this.whitOutSession();
 
-    fs.existsSync(this.SESSION_FILE_PATH)
-      ? this.whitSession()
-      : this.whitOutSession();
+    this.authFail();
+    this.clientReady();
 
-    // this.client.on('ready', () => {
-    //   console.log('Client is ready!');
-    // });
+    this.client.initialize();
 
-    // this.client.on('message', (msg) => {
-    //   if (msg.body == 'ping') {
-    //     this.client.sendMessage(msg.from, 'pong');
-    //     // msg.reply('pong');
-    //   }
-    // });
-
-    // await this.client.initialize();
     return { message: 'Client is ready!' };
   }
 
   //#region methods
-  private whitSession() {
+  private async whitSession() {
     console.log('whitSession');
+    this.client = new Client({
+      authStrategy: new LocalAuth(),
+      puppeteer: { headless: true },
+    });
+
+    this.authFail();
   }
 
-  private whitOutSession() {
+  private async whitOutSession() {
     console.log('whitOutSession');
+
+    this.client = new Client({});
     this.client.on('qr', (qr) => {
       qrcode.generate(qr, { small: true });
     });
 
     this.client.on('authenticated', (session) => {
       this.sessionData = session;
+      // TODO: guardar la session
       console.log(session);
-      fs.writeFile(this.SESSION_FILE_PATH, JSON.stringify(session), (err) => {
-        if (err) {
-          console.error(err);
-        }
-      });
     });
-
-    this.client.initialize();
   }
+
+  private authFail() {
+    this.client.on('auth_failure', (err) => {
+      console.log(err);
+    });
+  }
+  private clientReady() {
+    this.client.on('ready', () => {
+      console.log('Client is ready!');
+      this.listenMessages();
+    });
+  }
+
+  private listenMessages() {
+    this.client.on('message', (msg) => {
+      console.log(msg);
+      const { from, to, body, reply } = msg;
+      // this.historyMesages()
+      this.sendMessage(from, 'test mensage');
+      this.replyMessage(reply, 'mensaje respuesta');
+    });
+  }
+
+  private sendMedia(to: string, file: string) {
+    const mediaFile = MessageMedia.fromFilePath(`./media/${file}`);
+    this.client.sendMessage(to, mediaFile);
+  }
+
+  private sendMessage(to: string, message: string) {
+    this.client.sendMessage(to, message);
+  }
+  private replyMessage(reply: ReplyMethod, message: string) {
+    reply(message);
+  }
+
+  private historyMesages(number: string, message: string) {
+    // TODO: guardar en base
+    const today = moment().format(FORMATDATE);
+  }
+
   //#emdregion methods
 
   // create(createBotWebwhatsapDto: CreateBotWebwhatsapDto) {
@@ -81,3 +122,86 @@ export class BotWebwhatsapService {
   //   return `This action removes a #${id} botWebwhatsap`;
   // }
 }
+
+//#region interface
+interface ReplyMethod {
+  (
+    content: WAWebJS.MessageContent,
+    chatId?: string,
+    options?: WAWebJS.MessageSendOptions
+  ): Promise<WAWebJS.Message>;
+}
+//#endregion interface
+
+// Message {
+//   _data: {
+//     id: {
+//       fromMe: false,
+//       remote: '593992118720@c.us',
+//       id: '3EB0266581CC83F055F1',
+//       _serialized: 'false_593992118720@c.us_3EB0266581CC83F055F1'
+//     },
+//     body: 'jola mundo',
+//     type: 'chat',
+//     t: 1661995241,
+//     notifyName: 'Lilia',
+//     from: '593992118720@c.us',
+//     to: '593939384904@c.us',
+//     self: 'in',
+//     ack: 1,
+//     isNewMsg: true,
+//     star: false,
+//     kicNotified: false,
+//     recvFresh: true,
+//     isFromTemplate: false,
+//     pollInvalidated: false,
+//     broadcast: false,
+//     mentionedJidList: [],
+//     isVcardOverMmsDocument: false,
+//     isForwarded: false,
+//     hasReaction: false,
+//     ephemeralOutOfSync: false,
+//     productHeaderImageRejected: false,
+//     lastPlaybackProgress: 0,
+//     isDynamicReplyButtonsMsg: false,
+//     isMdHistoryMsg: false,
+//     requiresDirectConnection: false,
+//     pttForwardedFeaturesEnabled: true,
+//     isEphemeral: false,
+//     isStatusV3: false,
+//     links: []
+//   },
+//   mediaKey: undefined,
+//   id: {
+//     fromMe: false,
+//     remote: '593992118720@c.us',
+//     id: '3EB0266581CC83F055F1',
+//     _serialized: 'false_593992118720@c.us_3EB0266581CC83F055F1'
+//   },
+//   ack: 1,
+//   hasMedia: false,
+//   body: 'jola mundo',
+//   type: 'chat',
+//   timestamp: 1661995241,
+//   from: '593992118720@c.us',
+//   to: '593939384904@c.us',
+//   author: undefined,
+//   deviceType: 'web',
+//   isForwarded: false,
+//   forwardingScore: 0,
+//   isStatus: false,
+//   isStarred: false,
+//   broadcast: false,
+//   fromMe: false,
+//   hasQuotedMsg: false,
+//   duration: undefined,
+//   location: undefined,
+//   vCards: [],
+//   inviteV4: undefined,
+//   mentionedIds: [],
+//   orderId: undefined,
+//   token: undefined,
+//   isGif: false,
+//   isEphemeral: false,
+//   links: []
+// }
